441d57449d29a89c2ee11d32ef0214f2a20d327c
halilclkgl <119314365+halilclkgl@users.noreply.github.com>
2024-02-08 13:44:26 +0300

    Initial commit

---
+++ Assets/Scripts/Audio/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public static BackgroundMusic instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
